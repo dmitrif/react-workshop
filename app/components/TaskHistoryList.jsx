47b0fdb39f36e4cdd188376f4721e6e3203e3aa5
Farkov, Dmitri <dfarkov@ebay.com>
2015-08-27 19:22:03 -0400

    Creating starting point

--- app/components/TaskHistoryList.jsx
+++ app/components/TaskHistoryList.jsx
@@ -4,21 +4,7 @@ var ElapsedTime = require('components/ElapsedTime');
 
 var TaskHistoryList = React.createClass({
 	render: function () {
-		var duration;
-
-		// var tasks = (<tr className="empty-row"><td>-</td><td>-</td><td>-</td><td>-</td></tr>);
 		var tasks = null;
-
-		if (this.props.tasks.size > 0) {
-			tasks = this.props.tasks.map(task => 
-				(<tr key={task.get('startTime')}>
-					<td>{task.get('name')}</td>
-					<td>{task.get('startTime').format('HH:mm:ss')}</td>
-					<td>{task.get('endTime').format('HH:mm:ss')}</td>
-					<td><ElapsedTime from={task.get('startTime')} to={task.get('endTime')} static={true} /></td>
-				</tr>)
-			);
-		}
 
 		return (
 			<div id="TaskHistoryList">
@@ -32,7 +18,6 @@ var TaskHistoryList = React.createClass({
 						</tr>
 					</thead>
 					<tbody>
-						{tasks}
 					</tbody>
 				</table>
 			</div>
--- app/components/TaskView.jsx
+++ app/components/TaskView.jsx
@@ -5,41 +5,24 @@ var TaskActions = require('actions/TaskActions');
 var ElapsedTime = require('components/ElapsedTime');
 
 var CurrentTaskView = React.createClass({
-	create: function () {
-		var taskName = React.findDOMNode(this.refs.taskName).value;
-		TaskActions.create(taskName);
-	},
-
-	reset: function () {
-		TaskActions.reset(this.props.task.get('id'));
-	},
-
-	complete: function () {
-		TaskActions.complete();
-	},
-
 	render: function () {
-		if (this.props.task == null) {
-			return (
-				<div id="TaskView" className="task-creation">
-               		<input type="text" id="TaskName" ref="taskName" placeholder="What are you working on?" />
-               		<button id="StartButton" onClick={this.create}><span className="icon-play"></span></button>
-				</div>
-			);
-		}
-
-		var startTime = Moment(this.props.task.get('startTime'));
-
 		return (
-			<div id="TaskView" className="task-current">
-           		<dl>
-           			<input value={this.props.task.get('name')} disabled="disabled" />
-           			<ElapsedTime from={startTime} />
-           			<button id="ResetTask" onClick={this.reset}><span className="icon-to-start" /></button>
-           			<button id="CompleteTask" onClick={this.complete}><span className="icon-ok" /></button>
-       			</dl>
+			<div id="TaskView" className="task-creation">
+           		<input type="text" id="TaskName" ref="taskName" placeholder="What are you working on?" />
+           		<button id="StartButton"><span className="icon-play"></span></button>
 			</div>
 		);
+
+		// return (
+		// 	<div id="TaskView" className="task-current">
+  //          		<dl>
+  //          			<input value={this.props.task.get('name')} disabled="disabled" />
+  //          			<ElapsedTime from={startTime} />
+  //          			<button id="ResetTask" onClick={this.reset}><span className="icon-to-start" /></button>
+  //          			<button id="CompleteTask" onClick={this.complete}><span className="icon-ok" /></button>
+  //      			</dl>
+		// 	</div>
+		// );
 	}
 });
 
