3f7db7ab8a1bbd3fac67639ceaf2018cd0278315
Farkov, Dmitri <dfarkov@ebay.com>
2015-09-02 16:41:17 -0400

    Removing parts for workshop

--- app/components/Stopwatch.jsx
+++ app/components/Stopwatch.jsx
@@ -6,34 +6,12 @@ var TaskView = require('components/TaskView');
 var TaskHistoryList = require('components/TaskHistoryList');
 
 var Stopwatch = React.createClass({
-	getInitialState: function () {
-	    return {
-	    	currentTask: null,
-	        tasks: []  
-	    };
-	},
-
-	componentDidMount: function () {
-		TaskStore.addListener(this.updateTasks);
-	},
-
-	componentWillUnmount: function () {
-		TaskStore.removeListener(this.updateTasks);	      
-	},
-
-	updateTasks: function () {
-		this.setState({
-			currentTask: TaskStore.getCurrentTask(),
-			tasks: TaskStore.getAll()
-		});
-	},
-
 	render: function () {
 		return (
 			<div id="Stopwatch">
 				<a id="Logo" title="TrackIT" href="/">TrackIT</a>
-				<TaskView task={this.state.currentTask} />
-                <TaskHistoryList tasks={this.state.tasks} />
+				<TaskView />
+                <TaskHistoryList />
 			</div>
 		);
 	}
--- app/components/TaskHistoryList.jsx
+++ app/components/TaskHistoryList.jsx
@@ -6,7 +6,7 @@ var TaskHistoryList = React.createClass({
 	render: function () {
 		var tasks = (<tr className="empty-row"><td>-</td><td>-</td><td>-</td><td>-</td></tr>);
 
-		if (this.props.tasks.length > 0) {
+		if (this.props.tasks !== undefined && this.props.tasks.length > 0) {
 			tasks = this.props.tasks.map(task => 
 				(<tr key={task.startTime}>
 					<td>{task.name}</td>
